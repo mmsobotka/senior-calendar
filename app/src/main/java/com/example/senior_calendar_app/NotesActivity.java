c210926b9e036a98c107a8249f79d525ac4a7af9
mkalinowski11 <michal.kalinowski321@gmail.com>
2023-06-15 01:08:55 +0200

    Corrected NotesActivity file.

--- app/src/main/java/com/example/senior_calendar_app/NotesActivity.java
+++ app/src/main/java/com/example/senior_calendar_app/NotesActivity.java
@@ -1,22 +1,58 @@
 package com.example.senior_calendar_app;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class NotesActivity extends AppCompatActivity {
+
 
     private Button button_calendar;
     private Button button_menu;
     private Button button_parametry;
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notes_window);
+        button_menu = (Button) findViewById(R.id.dodajButton);
+        button_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNote();
+            }
+        });
 
         button_calendar = (Button) findViewById(R.id.calenButton);
         button_calendar.setOnClickListener(new View.OnClickListener() {
@@ -41,7 +77,64 @@ public class NotesActivity extends AppCompatActivity {
                 openPomiaryActivity();
             }
         });
+        ListView listView = findViewById(R.id.listView);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+
+        if (set == null) {
+
+            notes.add("Example note");
+        } else {
+            notes = new ArrayList(set);
+        }
+
+        // Using custom listView Provided by Android Studio
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, notes);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                // Going from MainActivity to NotesEditorActivity
+                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+                intent.putExtra("noteId", i);
+                startActivity(intent);
+
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int itemToDelete = i;
+                // To delete the data from the App
+                new AlertDialog.Builder(NotesActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Usunięcie notatki")
+                        .setMessage("Czy na pewno chcesz usunąć wybraną notatkę?")
+                        .setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+                                HashSet<String> set = new HashSet(NotesActivity.notes);
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                            }
+                        }).setNegativeButton("Nie", null).show();
+                return true;
+            }
+        });
     }
+
+    public void addNote() {
+        Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+        startActivity(intent);
+    }
+
     public void backToMenuActivity() {
         Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
