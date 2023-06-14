1db7ce79a5ea643d563fe60447147bf81e45225a
msobotka <milenasobotka@gmail.com>
2023-06-15 00:45:45 +0200

    Correct layout

--- app/src/main/java/com/example/senior_calendar_app/NotesActivity.java
+++ app/src/main/java/com/example/senior_calendar_app/NotesActivity.java
@@ -1,21 +1,18 @@
 package com.example.senior_calendar_app;
 
-import android.annotation.SuppressLint;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class NotesActivity extends AppCompatActivity {
-
 
     private Button button_calendar;
     private Button button_menu;
     private Button button_parametry;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +42,6 @@ public class NotesActivity extends AppCompatActivity {
             }
         });
     }
-
     public void backToMenuActivity() {
         Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
