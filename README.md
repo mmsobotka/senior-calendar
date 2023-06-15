461e829b635017522ea900bdce137ecad6e0d9b1
mkalinowski11 <michal.kalinowski321@gmail.com>
2023-06-15 19:54:20 +0200

    Updated PHP scripts and query

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Instrukcja uruchomienia projektu:
 5. Wchodzimy w zakładkę phpMyAdmin w górnym pasku po prawej stronie na samym końcu
 6. Po lewej są opcje tworzenia bazy danych, klikamy `New` i tworzymy nową bazę danych z bazowymi ustawieniami o nazwie `senior-calendar`
 7. Wchodzimy w naszą bazę danych i wchodzimy w zakładkę SQL, znajdującą się w górnym panelu.
-8. Tworzymy nową tabelę w bazie komendą: `CREATE TABLE users_data ( TIMESTAMP date, USER_ID text, TEMPERATURE float, WEIGHT float, BLOOD_PRESSURE int, SUGAR_LEVEL int);` i klikamy `Go`
+8. Tworzymy nową tabelę w bazie komendą: `CREATE TABLE data ( TIMESTAMP date, USER_ID text, TEMPERATURE float, SUGAR_LEVEL float, WEIGHT int, BLOOD_PRESSURE int, HEART_RATE int, SATURATION int);` i klikamy `Go`
 9. Następnie za pomocą komendy `ipconfig` pobieramy nasze IP z `IPv4 Address`, IP przyda się do zapisania w projekcie
 10. Zapisane IP przypisujemy do zmiennej w projekcie w interfejsie `ProjectVariables` do IPv4 z dopiskiem `:80`, przykładowa wartość zmiennej: `192.168.0.229:80`
 11. Uruchamiamy projekt w emulatorze, dodając nowe dane do bazy za pomocą przycisku `zapisz pomiar` warto pamiętać o warunkach przyjęcia inputu z funkcji `validate_data` z klasy MainActivity3. Strukturę tabeli oraz zapisane dane
--- server-scripts/add_data.php
+++ server-scripts/add_data.php
@@ -3,14 +3,16 @@
 $user = 'root';
 $password = '';
 $db_name = 'senior-calendar';
-$table_name = 'users_data';
+$table_name = 'data';
 
 $COL1 = "TIMESTAMP";
 $COL2 = "USER_ID";
 $COL3 = 'TEMPERATURE';
-$COL4 = 'WEIGHT';
-$COL5 = 'BLOOD_PRESSURE';
-$COL6 = 'SUGAR_LEVEL';
+$COL4 = 'SUGAR_LEVEL';
+$COL5 = 'WEIGHT';
+$COL6 = 'BLOOD_PRESSURE';
+$COL7 = 'HEART_RATE';
+$COL8 = 'SATURATION';
 
 $conn = new mysqli('localhost', $user, $password, $db_name);
 if (!$conn) {
@@ -26,11 +28,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $date_time = $_REQUEST[$COL1];
   $user_id = $_REQUEST[$COL2];
   $temperature = $_REQUEST[$COL3];
-  $weight =  $_REQUEST[$COL4];
-  $blood_pressure = $_REQUEST[$COL5];
-  $sugar_level = $_REQUEST[$COL6];
+  $sugar_level = $_REQUEST[$COL4];
+  $weight = $_REQUEST[$COL5];
+  $blood_pressure = $_REQUEST[$COL6];
+  $heart_rate = $_REQUEST[$COL7];
+  $saturation = $_REQUEST[$COL8];
 
-  $QUERY = "INSERT INTO $table_name ($COL1, $COL2, $COL3, $COL4, $COL5, $COL6) VALUES ('$date_time', '$user_id', $temperature, $weight, $blood_pressure, $sugar_level)";
+  $QUERY = "INSERT INTO $table_name ($COL1, $COL2, $COL3, $COL4, $COL5, $COL6, $COL7, $COL8) VALUES ('$date_time', '$user_id', $temperature, $sugar_level, $weight, $blood_pressure, $heart_rate, $saturation)";
   if ($conn->query($QUERY) === TRUE) {
         echo "New record added successfully";
   } else {
@@ -40,4 +44,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 mysqli_close($conn);
 ?>
-
